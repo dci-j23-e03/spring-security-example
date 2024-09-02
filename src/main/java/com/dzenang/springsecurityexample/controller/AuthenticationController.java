c2f395d46a0f6a413a12d82be42d768ccfee4d15
dzenang <dzenan.granulo@gmail.com>
2024-09-02 10:04:55 +0200

    Custom login form

--- src/main/java/com/dzenang/springsecurityexample/controller/AuthenticationController.java
+++ src/main/java/com/dzenang/springsecurityexample/controller/AuthenticationController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController {
+public class AuthenticationController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello";
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 }
--- src/main/java/com/dzenang/springsecurityexample/security/AppSecurityConfig.java
+++ src/main/java/com/dzenang/springsecurityexample/security/AppSecurityConfig.java
@@ -2,12 +2,14 @@ package com.dzenang.springsecurityexample.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +26,15 @@ public class AppSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated());
+
+        http.formLogin(auth -> auth.loginPage("/login").permitAll());
+        return http.build();
     }
 }
